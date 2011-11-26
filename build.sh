f8d232735ff315193292b96ad068af758fbc385f
David Anderson <dave@natulte.net>
2011-11-25 22:39:22 -0800

    change directory structure and build script to accomodate goinstall's logic

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export GOPATH=$(readlink -m `dirname $0`)
-
+mkdir -p src/mksysimage.googlecode.com/hg/mksysimage
+ln -f $GOPATH/main.go src/mksysimage.googlecode.com/hg/mksysimage/main.go
 find $GOPATH/src -name '*.go' | xargs gofmt -w
 goinstall mksysimage.googlecode.com/hg/mksysimage
