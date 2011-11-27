7e4a8c4c57a763115a555dca3aeebd233ebc8696
David Anderson <dave@natulte.net>
2011-11-26 18:04:55 -0800

    Restructure directory to make goinstall less sad.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export GOPATH=$(readlink -m `dirname $0`)
-mkdir -p src/mksysimage.googlecode.com/hg/mksysimage
-ln -f $GOPATH/main.go src/mksysimage.googlecode.com/hg/mksysimage/main.go
+mkdir -p src/mksysimage.googlecode.com/hg
+ln -sf $GOPATH/mksysimage src/mksysimage.googlecode.com/hg/mksysimage
 find $GOPATH/src -name '*.go' | xargs gofmt -w
 goinstall -nuke mksysimage.googlecode.com/hg/mksysimage
